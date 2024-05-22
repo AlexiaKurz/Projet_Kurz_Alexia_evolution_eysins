3283ef9056d5640e5c9676265b32dcc3f1e7a37a
AlexiaKurz <148451429+AlexiaKurz@users.noreply.github.com>
2024-05-22 21:15:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Initialisation de la carte avec une vue centrée et zoomée
-var mymap = L.map('map').setView([46.38097, 6.20653], 14);
+var mymap = L.map('map').setView([46.38, 6.20], 14);
 
 // Limitation de la carte à XXX
 mymap.setMaxBounds([[46.30, 6.10], [46.40, 6.30]]);
@@ -18,7 +18,7 @@ var esriImagery = L.tileLayer(
 
 osmLayer.addTo(mymap);
 
-// Affichage des coordonnées lorsque la souris se déplace sur la carte
+// Affichage des coordonnées lorsque la souris survole la carte
 var coordsDiv = document.getElementById('coords');
 
 mymap.on('mousemove', function (e) {
@@ -31,11 +31,7 @@ var accueilLink = document.getElementById('accueil-link');
 
 // Ajout d'un gestionnaire d'événements pour le clic sur le lien "Accueil"
 accueilLink.addEventListener('click', function(event) {
-
-  // Empêche le comportement par défaut du lien (évitez de le suivre)
   event.preventDefault();
-  
-  // Redirige l'utilisateur vers la page principale 
   window.location.href = 'index.html';
 });
 
@@ -47,13 +43,13 @@ var baseLayers = {
 var overlays = {};
 L.control.layers(baseLayers, overlays).addTo(mymap);
 
-// FOnction pour la barre de défilement ; changements selon l'année
 
+// Mise à jour du curseur de la barre slide et des infos associées
 function updateSlider(year) {
   document.getElementById('yearSlider').value = year;
   document.getElementById('slider_value').innerText = year;
-  toggleYear(year);
-  infoBox2.update(data_eysins, year);
+  toggleYear(year); //permet d'activer le changement en fonction de l'année
+  infoBox2.update(data_eysins, year); // mise à jour des infos en fonction de l'évolution de la populatione et du bâti
 }
 
 function toggleBuildingByYear() {
@@ -61,42 +57,43 @@ function toggleBuildingByYear() {
   updateSlider(year);
 }
 
-// Permet d'afficher les couches (4) selon les données disponibles
+// Permet de gérer les données en fonction de l'année et de donner des conditions
 function toggleYear(year) {
   var data;
-  if (parseInt(year)<1990) {
+  if (parseInt(year) < 1990) {
     data = null;
     infoBox3.update('Données indisponibles')
   }
 
-  if (parseInt(year)>=1990) {
+  if (parseInt(year) >= 1990) {
     data = BAT_Eysins1990;
     infoBox3.update('Couche année 1990')
   }
 
-  if (parseInt(year)>=2000) {
+  if (parseInt(year) >= 2000) {
     data = BAT_Eysins2000;
     infoBox3.update('Couche année 2000')
   }
 
-  if (parseInt(year)>=2010) {
+  if (parseInt(year) >= 2010) {
     data = BAT_Eysins2010;
     infoBox3.update('Couche année 2010')
   }
 
-  if (parseInt(year)>=2021) {
+  if (parseInt(year) >= 2021) {
     data = BAT_Eysins2021;
     infoBox3.update('Couche année 2021')
   }
-  
+
   geojsonLayer.clearLayers();
 
-  if (data) { 
+// Permet d'afficher les données si elles sont disponibles
+  if (data) {
     geojsonLayer.addData(data);
   }
 }
 
-// Permet d'afficher la couche du bâti en rouge
+// Création d'une nouvelle couche pour afficher les données
 var geojsonLayer = new L.geoJSON(null, {
   style: function (feature) {
     return { color: 'red', weight: 1 };
@@ -112,7 +109,7 @@ infoBox.onAdd = function (map) {
   return this._div;
 };
 
-// Informations de la commune provenant de communeEysins.js
+// Mise à jour des informations en fonction des données
 infoBox.update = function (props) {
   this._div.innerHTML = '<h4>Informations générales</h4>' + (props ?
     '<b>Commune: </b>' + props.NAME + '<br />' +
@@ -128,64 +125,60 @@ infoBox.addTo(mymap);
 // Création de la commune sur la carte
 var commune = L.geoJSON(CommuneEysins, {
   style: function (feature) {
-    return { color: 'green', weight: 1 }; // style de la commune 
+    return { color: 'green', weight: 1 };
   },
-
   onEachFeature: function (feature, layer) {
     var properties = feature.properties;
-
-    // Crée une petite bulle avec le nom de la commune
     layer.bindTooltip(properties.NAME).openTooltip();
-    
     layer.on('click', function (e) {
       var html = "<h4>Informations</h4>"
       infoBox.update(html);
-
       infoBox._div.style.right = '10px';
       zoomSurCommuneEtInfo(properties);
     });
   }
 }).addTo(mymap);
 
-// Action pour zoomer sur la commune lorsqu'elle est cliquée
+// Permet de zoomer sur la commune après un clic dessus
 commune.on('click', function (event) {
   mymap.fitBounds(event.target.getBounds());
 });
 
-// Fonction pour ajuster la vue de la carte et afficher les informations d'une commune
 function zoomSurCommuneEtInfo(properties) {
   mymap.fitBounds(commune.getBounds());
   infoBox.update(properties);
   infoBox._div.style.right = '10px';
 }
 
-// Création de l'infobox avec infos (densité, population, année et superficie)
-// N'apparait que lorsque l'on fait bouger la barre de défilement ou si on clique sur les informations de la première infobox
-
+// Création de l'infobox avec infos densité, population, année
 var infoBox2 = L.control();
 infoBox2.onAdd = function (map) {
   this._div = L.DomUtil.create('div', 'info');
+  this._div.innerHTML = '<p style="text-align: justify;">Cliquez sur "Evolution du bâti et de la densité de la population" pour avoir des informations complémentaires</p>';
   return this._div;
 };
 
 infoBox2.update = function (density, year) {
-  for (i=0; i<80; i++){
-    if (parseInt(density.features[i].properties.Annee)== parseInt(year)){
+  for (i = 0; i < 80; i++) {
+    if (parseInt(density.features[i].properties.Annee) == parseInt(year)) {
       break;
     }
   }
 
-  this._div.innerHTML = '<h4>Densité de la population</h4>' + 
-    '<b>Année: </b>' +  density.features[i].properties.Annee + '<br />' +
-    '<b>Population: </b>' + density.features[i].properties.Population + '<br />' +
+  var population = density.features[i].properties.Population;
+  var densite = density.features[i].properties.Densite;
+
+// Pour les données indisponibles, soit en null dans le fichier, un message apparait. 
+  this._div.innerHTML = '<h4>Densité de la population</h4>' +
+    '<b>Année: </b>' + density.features[i].properties.Annee + '<br />' +
+    '<b>Population: </b>' + (population ? population : 'Données indisponibles') + '<br />' +
     '<b>Superficie [km²]: </b>' + density.features[i].properties.Superficie + '<br />' +
-    '<b>Densité [hab/km²]: </b>' + density.features[i].properties.Densite + '<br />'
+    '<b>Densité [hab/km²]: </b>' + (densite ? densite : 'Données indisponibles') + '<br />';
 };
 
 infoBox2.addTo(mymap);
 
 // Création de l'infobox couche bâtiment disponible
-// Permet d'afficher l'année de la couche qui se trouve sur la carte
 var infoBox3 = L.control();
 infoBox3.onAdd = function (map) {
   this._div = L.DomUtil.create('div', 'info');
@@ -193,8 +186,8 @@ infoBox3.onAdd = function (map) {
 };
 
 infoBox3.update = function (year) {
-  this._div.innerHTML = '<h4>Infos : couches bâti</h4>' + 
-    '<b>Année: </b>' +  year + '<br />'
+  this._div.innerHTML = '<h4>Infos : couches bâti</h4>' +
+    '<b>Année: </b>' + year + '<br />';
 };
 
 infoBox3.addTo(mymap);
